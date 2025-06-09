7aaa5a29969a6583805b6cc55214f5af4fe74cd7
Darshan S <darshans2727@gmail.com>
2025-06-09 19:01:57 +0530

    added gemini response to / route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
 
 dotenv.config();
 
@@ -9,15 +10,25 @@ app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
-
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-app.get("/", (req: Request, res: Response) => {
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+app.get("/", async (req: Request, res: Response) => {
     if (!GEMINI_API_KEY) {
         res.status(500).send("GEMINI_API_KEY is not set");
         return;
     }
-    res.send("Server is running, GEMINI_API_KEY is set");
+
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: "Which model are you?",
+    });
+
+    res.send({
+        message: "Server is running, GEMINI_API_KEY is set",
+        gemini_response: response?.text,
+    });
 });
 
 app.listen(PORT, () => {
