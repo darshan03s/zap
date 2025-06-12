dddf570d573bf6415290540ff042b92d4cfdc583
Darshan S <darshans2727@gmail.com>
2025-06-12 13:58:17 +0530

    replaced monaco editor with code mirror

---
+++ frontend/src/features/react-wc-workspace/editor-and-preview/CodeMirrorEditor.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import CodeMirror, { type Extension } from '@uiw/react-codemirror';
+import * as themes from '@uiw/codemirror-themes-all';
+import { acceptCompletion, autocompletion } from '@codemirror/autocomplete';
+import { EditorView, keymap } from '@codemirror/view';
+import { langs } from '@uiw/codemirror-extensions-langs';
+import { useDarkMode } from '@/features/dark-mode/useDarkMode';
+import { useWebContainer } from '../webcontainer/useWebContainer';
+import { mapExtToLanguage } from './utils';
+
+const CodeMirrorEditor = () => {
+    const { selectedFile, webContainer } = useWebContainer();
+    const [value, setValue] = useState("");
+    const [language, setLanguage] = useState<string>("");
+
+    useEffect(() => {
+        if (!selectedFile) return;
+        const language = mapExtToLanguage(selectedFile?.name.split(".").pop() || "");
+        const value = selectedFile?.contents || ``;
+        setLanguage(language);
+        setValue(value);
+    }, [selectedFile]);
+
+
+    const extensions = [
+        // Languages
+        langs.tsx(),
+        langs.jsx(),
+        langs.javascript(),
+        langs.typescript(),
+        langs.html(),
+        langs.css(),
+        langs.json(),
+        langs.yaml(),
+        langs.markdown(),
+        langs.xml(),
+        langs.sass(),
+        langs.python(),
+        // Features
+        autocompletion({
+            closeOnBlur: false,
+            defaultKeymap: true,
+            icons: true
+        }),
+        keymap.of([
+            { key: 'Tab', run: acceptCompletion }
+        ]),
+        EditorView.lineWrapping,
+    ];
+    const { isDarkMode } = useDarkMode();
+    const [theme, setTheme] = useState<"light" | "dark" | "none" | Extension>(themes.vscodeLight);
+
+    useEffect(() => {
+        setTheme(isDarkMode ? themes.vscodeDark : themes.vscodeLight);
+    }, [isDarkMode]);
+
+    const onChange = (val: string) => {
+        setValue(val || "");
+        if (selectedFile) {
+            webContainer?.fs.writeFile(selectedFile.path, val || "");
+        }
+    };
+
+    return <CodeMirror
+        value={value}
+        height="600px"
+        extensions={extensions}
+        onChange={onChange}
+        lang={language}
+        basicSetup={{
+            autocompletion: true,
+            lineNumbers: true,
+            foldGutter: true,
+            allowMultipleSelections: true,
+            syntaxHighlighting: true,
+        }}
+        theme={theme} />;
+}
+
+export default CodeMirrorEditor;
--- frontend/src/features/react-wc-workspace/editor-and-preview/Editor.tsx
+++ frontend/src/features/react-wc-workspace/editor-and-preview/Editor.tsx
@@ -1,56 +1,16 @@
-import { Editor as MonacoEditor } from '@monaco-editor/react';
-import { useWebContainer } from '../webcontainer/useWebContainer';
-import { useState, useEffect } from 'react';
-import { useDarkMode } from '@/features/dark-mode/useDarkMode';
-import { mapExtToLanguage } from './utils';
+import { useWebContainer } from "../webcontainer/useWebContainer";
+import CodeMirrorEditor from "./CodeMirrorEditor";
 
 const Editor = () => {
-    const { selectedFile, webContainer } = useWebContainer();
-    const [language, setLanguage] = useState<string>("plaintext");
-    const [value, setValue] = useState<string>("");
-    const { isDarkMode } = useDarkMode();
-
-    function handleEditorChange(value: string | undefined) {
-        setValue(value || "");
-        if (selectedFile) {
-            webContainer?.fs.writeFile(selectedFile.path, value || "");
-        }
-    }
-
-    useEffect(() => {
-        if (!selectedFile) return;
-        const language = mapExtToLanguage(selectedFile?.name.split(".").pop() || "plaintext");
-        const value = selectedFile?.contents || ``;
-        setLanguage(language);
-        setValue(value);
-    }, [selectedFile]);
+    const { selectedFile } = useWebContainer();
 
     return (
         <div className="w-full h-full">
-            <MonacoEditor
-                key={selectedFile?.name}
-                height={'100vh'}
-                language={language}
-                theme={isDarkMode ? 'vs-dark' : 'vs'}
-                onChange={handleEditorChange}
-                value={value}
-                path={selectedFile?.path}
-                options={{
-                    wordWrap: "on",
-                    suggest: {
-                        showKeywords: true,
-                        showSnippets: true,
-                    },
-                    quickSuggestions: {
-                        other: true,
-                        comments: true,
-                        strings: true,
-                    },
-                    wordBasedSuggestions: "allDocuments",
-                    parameterHints: { enabled: true },
-                    fontFamily: "Cascadia Code",
-                }}
-            />
+            {selectedFile ?
+                <CodeMirrorEditor />
+                :
+                null
+            }
         </div>
     );
 };
