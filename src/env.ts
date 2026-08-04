3bf31fc9f2eabcf2e4da0596ac92c3ff6a0cd30b
Darshan S <darshans2727@gmail.com>
2026-08-04 15:43:01 +0530

    feat: add drizzle orm

--- src/env.ts
+++ src/env.ts
@@ -3,12 +3,14 @@ import * as z from 'zod'
 
 export const env = createEnv({
   server: {
-    APP_URL: z.string().min(1).default('http://localhost:3000')
+    APP_URL: z.string().min(1).default('http://localhost:3000'),
+    DATABASE_URL: z.string()
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1).default('http://localhost:3000')
   },
   runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
     APP_URL: process.env.APP_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL
   }
