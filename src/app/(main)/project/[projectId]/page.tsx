e2a0c22a0f5170458f5a9397e4a3bf7e8330fd46
Darshan S <darshans2727@gmail.com>
2026-09-01 20:13:15 +0530

    feat: add project page

---
+++ src/app/(main)/project/[projectId]/page.tsx
@@ -0,0 +1,7 @@
+const Page = async ({ params }: { params: Promise<{ projectId: string }> }) => {
+  const { projectId } = await params
+
+  return <div>Project {projectId}</div>
+}
+
+export default Page
