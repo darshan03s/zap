195b43eec411d437c52f081768108028b6fec68d
Darshan S <darshans2727@gmail.com>
2026-09-03 15:05:46 +0530

    refactor: update workspace layout and styling for improved responsiveness

--- src/components/workspace.tsx
+++ src/components/workspace.tsx
@@ -33,9 +33,9 @@ export const Workspace = () => {
   }
 
   return (
-    <Main className="grid grid-cols-12 gap-2 flex-1">
-      <div className="col-span-5 flex flex-col gap-2 px-3">
-        <Conversation className="border-2 flex flex-col max-h-122 min-h-127 rounded-lg">
+    <Main className="flex gap-2 flex-1 max-h-(--main-full-height)">
+      <div className="w-5/12 flex flex-col h-full p-4 gap-2">
+        <Conversation className="border-2 rounded-lg">
           <ConversationContent>
             {messages.map((message) => (
               <Message from={message.role} key={message.id}>
@@ -67,7 +67,7 @@ export const Workspace = () => {
         />
       </div>
 
-      <div className="col-span-8"></div>
+      <div className="flex-1"></div>
     </Main>
   )
 }
