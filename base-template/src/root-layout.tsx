fe7ce38271e9a0ce4b26671116b7bc47c8571ed4
Darshan S <darshans2727@gmail.com>
2026-09-11 13:01:32 +0530

    chore: remove header from base template

--- base-template/src/root-layout.tsx
+++ base-template/src/root-layout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router'
-import { Header } from './components/header'
 
 export const RootLayout = () => {
   return (
     <>
-      <Header />
       <Outlet />
     </>
   )
